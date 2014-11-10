75421d8785e65cbc29c04c17024d2494645e38ab
Peter Bacon Darwin <pete@bacondarwin.com>
2014-11-10 09:27:44 +0000

    step-8 phone details view
    - Fetch data for and render phone detail view
      - PhoneDetailCtrl controller to fetch details json with [$http] for a specific
        phone
      - template for the phone detailed view
    - CSS to make the phone details page look "pretty"

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -21,7 +21,9 @@ phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', 'Ph
     };
   }]);
 
-phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams',
-  function($scope, $routeParams) {
-    $scope.phoneId = $routeParams.phoneId;
+phonecatControllers.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
   }]);
